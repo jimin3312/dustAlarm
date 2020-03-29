5fa21d9b04c98e981fd66bb25d5341f7b5d4dd5a
jimin <jimin003312@gmail.com>
2020-03-29 22:12:27 +0900

    begin project

---
+++ app/src/main/java/com/example/dustalarm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dustalarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+const val KEY =  "3ahny5aBOqdFAnQlYaGDLnLLd3QyV3ORoXp6Aml886Qdp%2FbPCb4rqir5r8IjeWJHnT4HykKItVW2Mv2SIFhvdg%3D%3D"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

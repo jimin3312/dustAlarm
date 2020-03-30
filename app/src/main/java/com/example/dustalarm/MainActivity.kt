18fd3bd0725ce1dcf5af42cd33f21901603af4c4
jimin <jimin003312@gmail.com>
2020-03-30 22:54:09 +0900

    dust information

--- app/src/main/java/com/example/dustalarm/MainActivity.kt
+++ app/src/main/java/com/example/dustalarm/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.dustalarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 
 const val KEY =  "3ahny5aBOqdFAnQlYaGDLnLLd3QyV3ORoXp6Aml886Qdp%2FbPCb4rqir5r8IjeWJHnT4HykKItVW2Mv2SIFhvdg%3D%3D"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Thread {
+            try {
+                val connection = URL("http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=경기&pageNo=1&numOfRows=10&ver=1.3&_returnType=json&ServiceKey="+KEY).openConnection() as HttpURLConnection
+                val data = connection.inputStream.bufferedReader().readText()
+                Log.d("TEST", data.toString())
+            } catch (e : Exception)
+            {
+                Log.d("Exception", e.toString())
+            }
+        }.start()
     }
 }
+

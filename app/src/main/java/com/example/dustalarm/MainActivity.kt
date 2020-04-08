652a77368dcdf060e88a68fe0ef14fa77f4b3e55
jimin <jimin003312@gmail.com>
2020-04-08 22:12:56 +0900

    permission

--- app/src/main/java/com/example/dustalarm/MainActivity.kt
+++ app/src/main/java/com/example/dustalarm/MainActivity.kt
@@ -31,17 +31,17 @@ class MainActivity : AppCompatActivity() {
 
         service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
 
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        )
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                REQUEST_CODE
-            )
-        else {
+//        if (ActivityCompat.shouldShowRequestPermissionRationale(
+//                this,
+//                android.Manifest.permission.ACCESS_FINE_LOCATION
+//            )
+//        )
+//            ActivityCompat.requestPermissions(
+//                this,
+//                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+//                REQUEST_CODE
+//            )
+//        else {
             buildLocationRequest()
             buildLocationCallBack()
 
@@ -57,27 +57,27 @@ class MainActivity : AppCompatActivity() {
 //                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
 //            })
 
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                    REQUEST_CODE
-                )
-            }
+//            if (ActivityCompat.checkSelfPermission(
+//                    this,
+//                    android.Manifest.permission.ACCESS_FINE_LOCATION
+//                ) != PackageManager.PERMISSION_GRANTED ||
+//                ActivityCompat.checkSelfPermission(
+//                    this,
+//                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                ActivityCompat.requestPermissions(
+//                    this,
+//                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+//                    REQUEST_CODE
+//                )
+//            }
             fusedLocationProviderClient.requestLocationUpdates(
                 locationRequest,
                 locationCallback,
                 Looper.myLooper()
             )
-        }
+
 
         DustNotiAlarm(this).register()
     }
@@ -121,30 +121,6 @@ class MainActivity : AppCompatActivity() {
         locationRequest.interval = 5000
         locationRequest.fastestInterval = 3000
         locationRequest.smallestDisplacement = 10f
-    }
-
-    override fun onPause() {
-        super.onPause()
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            REQUEST_CODE -> {
-                if (grantResults.size > 0) {
-                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                        Toast.makeText(this, "Permisson granted", Toast.LENGTH_SHORT)
-                            .show()
-                    else
-                        finish()
-                }
-            }
-        }
     }
 
 }
---
+++ app/src/main/java/com/example/dustalarm/PermissionActivity.kt
@@ -0,0 +1,62 @@
+package com.example.dustalarm
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+class PermissionActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.permission_layout)
+
+//        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+//            ActivityCompat.requestPermissions(
+//                this,
+//                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+//                MainActivity.REQUEST_CODE
+//            )
+//            Log.d("TEST","aaasdf2")
+//        }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                MainActivity.REQUEST_CODE
+            )
+        }
+        else
+            startActivity(Intent(this, MainActivity::class.java))
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MainActivity.REQUEST_CODE -> {
+                if (grantResults.size > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    {
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }
+                    else
+                        finish()
+                }
+            }
+        }
+    }
+}
